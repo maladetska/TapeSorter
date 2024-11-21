68e6303cb5d9a103c826d46f63f81e704ed8c351
Maria Okorochkova <okorochkova26@gmail.com>
2024-11-22 01:31:41 +0300

    renaming

--- lib/tape/delays/delays.hpp
+++ lib/tape/delays/delays.hpp
@@ -16,23 +16,23 @@ struct Delays {
   //////////////////////////////////////////////////////////////////////////////
   /// \brief Delays constructor by delays for read write and shift.
   ///
-  /// \param delay_for_read
-  /// \param delay_for_write
+  /// \param delay_for_reading
+  /// \param delay_for_writing
   /// \param delay_for_shift
   //////////////////////////////////////////////////////////////////////////////
-  Delays(std::chrono::milliseconds delay_for_read,
-         std::chrono::milliseconds delay_for_write,
+  Delays(std::chrono::milliseconds delay_for_reading,
+         std::chrono::milliseconds delay_for_writing,
          std::chrono::milliseconds delay_for_shift);
 
   //////////////////////////////////////////////////////////////////////////////
   /// \brief Delay in reading the element indicated by the magnetic head.
   //////////////////////////////////////////////////////////////////////////////
-  std::chrono::milliseconds delay_for_read_{};
+  std::chrono::milliseconds delay_for_reading_{};
   //////////////////////////////////////////////////////////////////////////////
   /// \brief Delay in putting the element to the position indicated by the
   /// magnetic head.
   //////////////////////////////////////////////////////////////////////////////
-  std::chrono::milliseconds delay_for_write_{};
+  std::chrono::milliseconds delay_for_writing_{};
   //////////////////////////////////////////////////////////////////////////////
   /// \brief Delay for moving the tape by one position.
   //////////////////////////////////////////////////////////////////////////////
